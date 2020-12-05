f225c62c0bcc801a1b2783e4b0cb1954b0f34ad5
Kimberly Ramirez <kim.ram27@gmail.com>
2020-12-05 08:28:09 +0000

    second chapter exercises

---
+++ chapter_2_exercises/exercise_5.rb
@@ -0,0 +1,3 @@
+# the first x will equal 3 because it was created in the outterscope
+# the second x will throw an error because it was first added for the firs time inside the scope.
+# the outterscope cannot see innerscope variables 
---
+++ chapter_2_exercises/name.rb
@@ -0,0 +1,6 @@
+# modified for exercise 3
+puts "What is your first name?"
+  first_name = gets.chomp
+puts "Thank you. What is your last name?"
+  last_name = gets.chomp
+puts "Awesome! Your full name is" + " " + first_name + " " + last_name
